df684a2a6b7ebfa7884093320f225b1975d9acab
rpsau <rpsaul18@outlook.com>
2022-03-30 22:48:18 -0600

    Consultas de los catalogos listas

--- Controllers/CatalogosController.cs
+++ Controllers/CatalogosController.cs
@@ -26,5 +26,118 @@ namespace API_Hospital_Boca.Controllers
                 throw;
             }
         }
+
+        [HttpGet ("hospitalBoca/catalogos/calidadServicio")]
+        public IActionResult getAllCalidadServicio()
+        {
+            try
+            {
+                var res = service.getAllCalidadServicio();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/escolaridad")]
+        public IActionResult getAllEscolaridad()
+        {
+            try
+            {
+                var res = service.getAllEscolaridad();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/estadoCivil")]
+        public IActionResult getAllEstadoCivil()
+        {
+            try
+            {
+                var res = service.getAllEstadoCivil();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/lugarReferencia")]
+        public IActionResult getAllLugarReferencia()
+        {
+            try
+            {
+                var res = service.getAllLugarReferencia();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/metodoPlanificacion")]
+        public IActionResult getAllMetodoPlanificacion()
+        {
+            try
+            {
+                var res = service.getAllMetodoPlanificacion();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/ocupacion")]
+        public IActionResult getAllOcupacion()
+        {
+            try
+            {
+                var res = service.getAllOcupacion();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/opinionPareja")]
+        public IActionResult getAllOpinionPareja()
+        {
+            try
+            {
+                var res = service.getAllOpinionPareja();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
+        [HttpGet ("hospitalBoca/catalogos/religion")]
+        public IActionResult getAllReligion()
+        {
+            try
+            {
+                var res = service.getAllReligion();
+                return Ok(res);   
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+        
     }
 }
