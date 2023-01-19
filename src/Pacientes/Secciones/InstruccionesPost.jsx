12950ca7d0719779285412bf1811ad75da703d6d
Jose_Varela <jo.lu6853@gmail.com>
2023-01-18 22:31:17 -0600

    Selección de doctor en instrucciones

--- src/Pacientes/Secciones/InstruccionesPost.jsx
+++ src/Pacientes/Secciones/InstruccionesPost.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
-import { AccordionDetails, TextField, Button, CircularProgress } from '@material-ui/core'
+import React, {useState, useEffect} from 'react'
+import { AccordionDetails, TextField, Button, FormControl, MenuItem, InputLabel, Select, CircularProgress } from '@material-ui/core'
 import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded'
 import { makeStyles } from '@material-ui/core/styles'
 import axios from 'axios'
+import clsx from 'clsx'
 
 const useStyles = makeStyles(theme => ({
   fullWidth: {
@@ -19,12 +20,25 @@ const useStyles = makeStyles(theme => ({
 export default function InstruccionesPost({expediente, token}) {
   const classes = useStyles()
   const [isLoad, togLoad] = useState(false)
+  const [doctores, setDoctores] = useState([])
+
+  const [datos, setDatos] = useState({
+    fkPersonal: '',
+    fkMedico: ''
+  })
+
+  const handleChange = event => {
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value,
+    })
+  }
 
   const GenerarInstrucciones = (event) => {
     event.preventDefault();
     togLoad(true)
 
-    const med = document.getElementById("medico").value
+    // const med = document.getElementById("medico").value
     const uni = document.getElementById("unidad").value
 
     axios
@@ -32,7 +46,7 @@ export default function InstruccionesPost({expediente, token}) {
     {
       // TODO: Aplicar relación por medio de Historia Clinica.
       pacienteID: expediente,
-      medicoResponsable: med,
+      medicoResponsable: datos.fkMedico,
       unidadMedica: uni,
     },{
       responseType: 'arraybuffer',
@@ -60,18 +74,53 @@ export default function InstruccionesPost({expediente, token}) {
 		)
   }
 
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_SERVIDOR + '/hospitalBoca/doctores/all', {
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': `Bearer ${token()}`,
+        },
+      })
+      .then(
+        response => {
+          if (response.status === 200) {
+            setDoctores(response.data)
+            // setErrorbd(false)
+          }
+        },
+        error => {
+          // if (!error.response) setErrorbd(true)
+        }
+      )
+  }, [token])
+
   return (
     <AccordionDetails>
       <form onSubmit={GenerarInstrucciones} autoComplete="off" className={classes.fullWidth}>
-          <TextField
-            id="medico"
-            variant="outlined"
-            fullWidth
-            required
-            disabled={isLoad}
-            className={classes.marginTop}
-            label="Médico responsable"
-          />
+          <FormControl variant="outlined" fullWidth className={clsx(classes.input)}>
+            <InputLabel id="fkMedico">Médico responsable</InputLabel>
+            <Select
+              required
+              labelId="fkMedico"
+              id="fkMedico"
+              variant="outlined"
+              name='fkMedico'
+              fullWidth
+              onChange={handleChange}
+              disabled={isLoad}
+              defaultValue=""
+              label="Médico responsable"
+            >
+              {doctores.map(n => {
+                return (
+                  <MenuItem value={n.idDoctor}>
+                    {n.nombre} {n.apPaterno} {n.apMaterno}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
 
           <TextField
             id="unidad"
