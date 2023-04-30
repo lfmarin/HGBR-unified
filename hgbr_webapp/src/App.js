0f467605ab64f40adfce7b06035099eb698f631e
Josue Téllez <josuetllezh@gmail.com>
2023-04-30 12:50:33 -0600

    Intentando hacer enlaces con las imagenes al form

--- hgbr_webapp/src/App.js
+++ hgbr_webapp/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import btn_urgencia from './media/urgencias.png';
+// import add_paciente from './components/pacientes/agregar_paciente';
+
+function BotonConImagen(props) {
+  return (
+    <button
+      style={{
+        backgroundImage: `url(${props.btn_urgencia})`,
+        backgroundSize: 'cover',
+        width: '200px',
+        height: '200px',
+        border: 'none',
+        borderRadius: '50%',
+        cursor: 'pointer',
+      }}
+      onClick={props.onClick}
+    />
+  );
+}
+
+// export default BotonConImagen;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <img width='200px' src="./media/urgencias.png" className="opt1" />
+        <img width='200px' src="./media/hospitalarios.png" className="opt2" />
+      </center>
     </div>
   );
 }
