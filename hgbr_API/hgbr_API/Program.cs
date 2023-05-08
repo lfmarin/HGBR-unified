94eb250a3030cf6c33e5a723de4ef98f0e9161b4
Josue Téllez <josuetllezh@gmail.com>
2023-05-08 13:34:49 -0600

    Ya se consumen la informacion de los catalogos !!!!

--- hgbr_API/hgbr_API/Program.cs
+++ hgbr_API/hgbr_API/Program.cs
@@ -1,4 +1,4 @@
-using API_Hospital_Boca.Bussiness;
+using hgbr_API.Interfaces;
 using hgbr_API.Models;
 using hgbr_API.ImplInterfaces;
 //using Google.Protobuf.WellKnownTypes;
@@ -10,10 +10,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Configuration;
-
-
+using API_Hospital_Boca.Bussiness;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 
@@ -29,6 +30,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(c => {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API-Hospital Boca", Version = "v1" });
 });
+
+
+builder.Services.AddDbContext<hgbr_API.Models.HgbrContext>(options => {
+    var connectionString = configuration.GetConnectionString("HospitalBocaConnectionString");
+    var version = ServerVersion.Parse("8.0.26-mysql");
+    options.UseMySql(connectionString, version);
+});
+
+
+builder.Services.AddScoped<ICatalogosServices, CatalogosServices>();
 
 builder.Services.AddControllers();
 
---
+++ hgbr_API/hgbr_API/Startup.cs
@@ -0,0 +1,26 @@
+﻿using hgbr_API.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace hgbr_API
+{
+    public class Startup
+    {
+        public Startup (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+    }
+}
