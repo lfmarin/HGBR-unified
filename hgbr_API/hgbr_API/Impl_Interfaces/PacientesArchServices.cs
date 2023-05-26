b41a10cd8c93c5ab51f767beb79e9af761e904e3
Josue Téllez <josuetllezh@gmail.com>
2023-05-26 14:47:44 -0600

    Ya quedo el contador . . .

--- hgbr_API/hgbr_API/Impl_Interfaces/PacientesArchServices.cs
+++ hgbr_API/hgbr_API/Impl_Interfaces/PacientesArchServices.cs
@@ -69,6 +69,7 @@ namespace hgbr_API.ImplInterfaces
             }
         }
 
+
         public object getPacientesarch(string numExpediente)
         {
             try
@@ -116,6 +117,7 @@ namespace hgbr_API.ImplInterfaces
                 return null;
             }
         }
+
 
         public void savePacientesarch(Pacientesarch paciente)
         {
@@ -240,5 +242,3 @@ namespace hgbr_API.ImplInterfaces
         }
     }
 }
-
-
