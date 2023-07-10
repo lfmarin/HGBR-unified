d6d971fcf0967c66d71ac3275c8ff37343737a70
Joel Jacome Pioquinto <joeljacomep@outlook.com>
2023-07-09 20:41:18 -0600

    Time and Date Only converters

--- hgbr_API/hgbr_API/Startup.cs
+++ hgbr_API/hgbr_API/Startup.cs
@@ -24,6 +24,7 @@ namespace hgbr_API
             writer.WriteStringValue(isoDate);
         }
     }
+
     public sealed class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
     {
         public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
@@ -37,6 +38,37 @@ namespace hgbr_API
             writer.WriteStringValue(isoDate);
         }
     }
+
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        private const string DateFormat = "yyyy-MM-dd";
+
+        public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return DateOnly.ParseExact((string)reader.Value, DateFormat, CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
+        }
+    }
+
+    public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
+    {
+        private const string TimeFormat = "HH:mm:ss.FFFFFFF";
+
+        public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return TimeOnly.ParseExact((string)reader.Value, TimeFormat, CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+        }
+    }
+    
     public class Startup
     {
         public Startup (IConfiguration configuration)
